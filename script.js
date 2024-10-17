71de85b434be2070169c95e897fdc7443a0732bd
Kjartan Heimisson <kheimisson@gmail.com>
2024-10-17 15:19:19 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // Write your code here
+
+const lightbulb1 = document.querySelector('#lightbulb1');
+const lightbulb2 = document.querySelector('#lightbulb2');
+const lightbulb3 = document.querySelector('#lightbulb3');
+const subtitle = document.querySelector('.subtitle')
+
+let count = 0;
+
+lightbulb1.addEventListener('click', function(event) {
+    count++;
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    this.classList.toggle('active')
+});
+lightbulb2.addEventListener('click', function(event) {
+    count++;
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    this.classList.toggle('active')
+});
+lightbulb3.addEventListener('click', function(event) {
+    count++;
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    this.classList.toggle('active')
+});
+
